bb3d05053e47155386767ebb17c816b38004f74d
jzide <jzide@JzBook>
2025-04-16 00:43:56 +0900

    update

--- 9.test.sh
+++ 9.test.sh
@@ -4,25 +4,17 @@
 while true; do
 read -p "SELECT [yes or no]: " _YKEY
 case $_YKEY in
-YES)
-Yes)
-Y)
-yes)
-y)
-echo ""
-echo "YES"
-echo ""
-exit 0
-;;
-NO)
-No)
-N)
-no)
-n)
-echo ""
-echo "NO"
-echo ""
-exit 0
-;;
+	YES | Yes | Y | yes | y)
+		echo ""
+		echo "YES"
+		echo ""
+		exit 0
+		;;
+	NO | No | N | no | n)
+		echo ""
+		echo "NO"
+		echo ""
+		exit 0
+		;;
 esac
 done
