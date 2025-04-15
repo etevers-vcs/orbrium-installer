eda89100a573ddc9aa3a914b42443fe6770d24ef
root <root@orb.esp.sdi>
2025-04-15 11:04:09 +0000

    exec

---
+++ erase.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export DEFAULT_DEBUG="false"
+
+echo "==================================================="
+echo "# ENVIRONMENTS                                    #"
+echo "==================================================="
+while true; do
+read -p "DOCKER_SOCK [default:/var/run/docker.sock]: " DOCKER_SOCK
+if [ -n "$DOCKER_SOCK" ]; then
+export DOCKER_SOCK=$DOCKER_SOCK
+else
+export DOCKER_SOCK="/var/run/docker.sock"
+fi
+if [ -e $DOCKER_SOCK ]; then
+echo "  --> $DOCKER_SOCK [ OK ]"
+break
+else
+echo "  --> $DOCKER_SOCK [ Failed ]"
+fi
+done
+echo ""
+
+docker run -ti --rm \
+	-v $DOCKER_SOCK:/var/run/docker.sock \
+	orbrium/install:$DEFAULT_DEBUG \
+	python pygma.py -e all
