9703c31eb498caba4cfbb8e76f7abcc2b28b6e3c
root <root@orb.esp.sdi>
2025-04-16 02:02:16 +0000

    exec

---
+++ 6.remove.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+WORKING_PATH=$(realpath $0)
+WORKING_DIR=$(dirname $WORKING_PATH)
+source $WORKING_DIR/stage.profile
+
+echo "==================================================="
+echo "# ENVIRONMENTS                                    #"
+echo "==================================================="
+while true; do
+read -p "DOCKER_SOCK [default:/var/run/docker.sock]: " DOCKER_SOCK
+if [ -n "$DOCKER_SOCK" ]; then
+export DOCKER_SOCK=$DOCKER_SOCK
+else
+export DOCKER_SOCK="/var/run/docker.sock"
+fi
+if [ -e $DOCKER_SOCK ]; then
+echo "  --> $DOCKER_SOCK [ OK ]"
+break
+else
+echo "  --> $DOCKER_SOCK [ Failed ]"
+fi
+done
+echo ""
+
+docker run -ti --rm \
+	-v $DOCKER_SOCK:/var/run/docker.sock \
+	orbrium/install:$DEFAULT_STAGE-active \
+	python pygma.py -e all
